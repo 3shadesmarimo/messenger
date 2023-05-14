ab87b89f4727ac4f46aace9660d5e14210cb0058
Bilegt <bilegtbd@gmail.com>
2023-05-14 00:41:46 -0400

    added a button

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import {BsGithub} from 'react-icons/bs';
+import {BsGoogle} from 'react-icons/bs';
+import Button from "@/app/components/button";
 import Input from "@/app/components/inputs/input";
 import { useCallback, useState } from "react";
-import { 
+import {
     FieldValues,
     SubmitHandler,
-    useForm } from "react-hook-form";
+    useForm
+} from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
 
 type Vairnet = 'LOGIN' | 'REGISTER';
 
@@ -14,7 +19,7 @@ const AuthForm = () => {
     const [isLoading, setIsLoading] = useState(false);
     const toggleVariant = useCallback(() => {
 
-        if(variant == 'LOGIN') {
+        if (variant == 'LOGIN') {
             setVariant('REGISTER');
         } else {
             setVariant('LOGIN');
@@ -36,7 +41,7 @@ const AuthForm = () => {
     });
 
 
-    const onSubmit : SubmitHandler<FieldValues> = (data) => {
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
 
         if (variant == 'REGISTER') {
@@ -69,12 +74,100 @@ const AuthForm = () => {
                 sm:rounded-lg
                 sm:px-10
             ">
-                <form 
+                <form
                     className="space-y-6"
                     onSubmit={handleSubmit(onSubmit)}
                 >
-                    <Input/>
+                    {variant == 'REGISTER' && (
+                        <Input
+                            id="email"
+                            label="Email"
+                            register={register}
+                            errors={errors}
+                            disabled={isLoading}
+                        />
+                    )}
+
+                    <Input
+                        id="email"
+                        label="Email address"
+                        type="email"
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}
+                    />
+
+                    <Input
+                        id="password"
+                        label="Password"
+                        type="password"
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}
+                    />
+
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >{variant ==  'LOGIN' ? 'Sign in' : 'Register'}</Button>
+                    </div>
+
+
                 </form>
+
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="
+                           absolute
+                           inset-0
+                           flex
+                           items-center 
+                        ">
+                            <div className="w-full border-t border-gray-300">
+
+                            </div>
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+
+
+                         <AuthSocialButton
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+
+                    </div>
+                </div>
+                <div className="
+                    flex
+                    gap-2
+                    justify-center
+                    text-sm
+                    mt-6
+                    px-2
+                    text-gray-500
+                ">
+                    <div>
+                        {variant == 'LOGIN' ? 'New to Messenger?' : 'Already have an account' }
+                    </div>
+                    <div
+                        onClick={toggleVariant}
+                        className='underline cursor-pointer'
+                    >
+                        {variant == 'LOGIN' ? 'Create an account' : 'Log In'}
+                    </div>
+                </div>
             </div>
         </div>
     );
